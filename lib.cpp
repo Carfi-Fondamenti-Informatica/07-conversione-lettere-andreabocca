806fd474e163f488b8c8967b57b8901bdfa12f24
andreabocca <127606319+andreabocca@users.noreply.github.com>
2023-05-21 17:50:09 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
 
+#include "lib.h"
+
+ char maiusc(char a){
+    if ((a>=97)&&(a<=122)){
+        return a - 32;
+    } else if ((a>=65)&&(a<=90)){
+        return a + 32;
+    } else {
+        return false;
+    }
+}
